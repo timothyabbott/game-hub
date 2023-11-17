caffd60d5d724c8993a7a04d8ea12a3f56107831
Tim Abbott <20151421+timothyabbott@users.noreply.github.com>
2023-11-17 12:55:32 +0000

    82. Creating a Response Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import { useState } from "react";
-import { Button, ButtonGroup } from "@chakra-ui/react";
 import "./App.css";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      // the base and lg properties specify the layout for stadnard and large screens
+      templateAreas={{
+        // back ticks are need due to the multiple double quotes
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Gold
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
