8422d68938a4571504072fe006c9271039b8aee2
Tim Abbott <20151421+timothyabbott@users.noreply.github.com>
2023-11-19 18:52:55 +0000

    89. Displaying Platform Icons

--- src/Hooks/useGames.ts
+++ src/Hooks/useGames.ts
@@ -2,35 +2,40 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
 export interface Game {
-    id: number;
-    name: string;
-    background_image:string
-  }
-  
-  interface FetchGameResponse {
-    count: number;
-    results: Game[];
-  }
-
-const useGames = ()=>{
-
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState("");
-  
-    useEffect(() => { 
-      const controller = new AbortController();
-      apiClient
-        .get<FetchGameResponse>("/games",{signal:controller.signal})
-        .then((result) => setGames(result.data.results))
-        .catch((error) => {
-            if (error instanceof CanceledError) return;
-            setError(error.message);
-        })
-        return () =>controller.abort()
-    },[]);
-
-    return{games, error}
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 
-export default useGames
+interface FetchGameResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+    apiClient
+      .get<FetchGameResponse>("/games", { signal: controller.signal })
+      .then((result) => setGames(result.data.results))
+      .catch((error) => {
+        if (error instanceof CanceledError) return;
+        setError(error.message);
+      });
+    return () => controller.abort();
+  }, []);
+
+  return { games, error };
+};
+
+export default useGames;
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Game } from "../Hooks/useGames";
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
 
 interface Props {
   game: Game;
@@ -12,6 +13,14 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize={"2xl"}>{game.name}</Heading>
+        <PlatformIconList
+          // we need to do this strange platform mapping because parent_platforms
+          // has a list of platforms which in turn has a platform property which is a list of platforms
+
+          platforms={game.parent_platforms.map(
+            (platforms) => platforms.platform
+          )}
+        ></PlatformIconList>
       </CardBody>
     </Card>
   );
