52ad5ba3dcd5ff8f7b7dd5143dad6ff692c663af
Tim Abbott <20151421+timothyabbott@users.noreply.github.com>
2023-12-17 10:47:17 +0000

    110. Cleaning up the Genres

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,5 +1,4 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
-import React from "react";
 import { BsChevronDown, BsChevronRight } from "react-icons/bs";
 import usePlatforms from "../Hooks/usePlatforms";
 import { Platform } from "../Hooks/useGames";
